57c7236a2a889f863b6e906ea8885ada540c8440
muhammadBilal5002 <57361001+muhammadBilal5002@users.noreply.github.com>
2022-08-03 16:06:28 +0500

    Update jwt.js

--- Utilities/jwt.js
+++ Utilities/jwt.js
@@ -3,7 +3,7 @@ exports.JWT = (user,res)=>{
     const Option = {
         // httpOnly:true,
         expires: new Date(Date.now() + process.env.Expires_Cookies*24*60*60*1000),
-         sameSite: 'None'
+      //   sameSite: 'None'
     }
     res.set('Access-Control-Allow-Credentials', 'true');
     res.set('Access-Control-Expose-Headers','date, etag, access-control-allow-origin, access-control-allow-credentials');
