785943630f03ab8135d2a3d0ac9c63303aafd114
muhammadBilal5002 <57361001+muhammadBilal5002@users.noreply.github.com>
2022-08-03 05:57:52 +0500

    Update app.js

--- app.js
+++ app.js
@@ -11,15 +11,21 @@ require("./Config/dbConfig")()
 const server = http.listen(PORT,()=>{
     console.log("Server Started At PORT "+PORT)
 })
-app.use(
-    cors({
-        origin: function (origin, callback) {
-            callback(null, origin);
-        },
-        credentials: true,
-    })
-);
-
+//app.use(
+ //   cors({
+       // origin: function (origin, callback) {
+       //     callback(null, origin);
+     //   },
+   //     credentials: true,
+   // })
+//);
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    next();
+});
 //uncaughtException
 process.on("uncaughtException",(e)=>{
     console.log(`Error in Server ${e.message}`)
