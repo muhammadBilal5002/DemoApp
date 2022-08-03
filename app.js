cfb2dfaf7364aff6209a1815d0c9eaeb5935c2ea
muhammadBilal5002 <57361001+muhammadBilal5002@users.noreply.github.com>
2022-08-03 06:08:27 +0500

    Update app.js

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', req.headers.origin);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    //res.setHeader('Access-Control-Allow-Headers', 'Set-Cookie')
     next();
 });
 //uncaughtException
